46889b0ab563f4e212f646316284f7c0e751bb3a
rubayet170746 <rafirubayet75@gmail.com>
2021-09-26 19:01:49 +0600

    added

--- SQL/Hello SQL World!.sql
+++ SQL/Hello SQL World!.sql
@@ -1,1 +1,14 @@
 SELECT 'hello world!' AS "Greeting"
+
+
+
+//below another version
+
+
+
+CREATE TABLE codewars (
+  Greeting varchar(50) NOT NULL
+);
+
+INSERT INTO codewars VALUES ('hello world!');
+SELECT * FROM codewars WHERE Greeting = 'hello world!';
